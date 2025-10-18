c566da3e8a815423748d7ff455c534eab70c4f58
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2025-10-18 20:11:23 -0300

    chore: (user) Incluir validação na requisição POST

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,23 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import type { CreateUserDto } from './dtos/createUser.dto';
 import { UserService } from './user.service';
 import { UserEntity } from './entities/user.entity';
+import { ReturnUserDto } from './dtos/returnUser.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get()
-  getAllUsers() {
-    return this.userService.getAllUser();
-  }
-
+  @UsePipes(ValidationPipe)
   @Post()
   createUser(@Body() createUser: CreateUserDto): Promise<UserEntity> {
     return this.userService.createUser(createUser);
+  }
+
+  @Get()
+  async getAllUsers(): Promise<ReturnUserDto[]> {
+    return (await this.userService.getAllUser()).map(
+      (UserEntity)=> new ReturnUserDto(UserEntity)
+    )
   }
 }
