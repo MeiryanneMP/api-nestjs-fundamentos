b9facc90a0231fd571e7d00aae612d9f3ccc08bd
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2025-10-21 18:36:35 -0300

    feat: jwt Token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secret: 'senhaMuitoGrandeParaNaoPerder', //adicionar uma env aq
+      signOptions: {expiresIn: '60s'}, //adicionar env aq tb
+    })
+  ],
   providers: [AuthService],
   controllers: [AuthController]
 })
