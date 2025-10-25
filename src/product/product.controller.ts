b737e2ddad9069ab8510aae0282196c8798b6302
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2025-10-25 17:12:34 -0300

    feat: busca lista de produtos

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { Roles } from 'src/decorators/roles.decorator';
 import { UserType } from 'src/user/enum/user-type.enum';
 import { ProductService } from './product.service';
-import { ReturnProduct } from './dtos/return-product.dto';
+import { ReturnProductDto } from './dtos/return-product.dto';
 
 @Roles(UserType.Admin, UserType.User)
 @Controller('product')
@@ -10,9 +10,9 @@ export class ProductController {
   constructor(private readonly productService: ProductService){}
 
   @Get()
-  async findAll(): Promise<ReturnProduct[]> {
+  async findAll(): Promise<ReturnProductDto[]> {
     return ( await this.productService.findAll()).map(
-      (product) => new ReturnProduct(product),
+      (product) => new ReturnProductDto(product),
     );
   }
 }
