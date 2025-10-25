1266f87672a4de8bf403e81512bcfc955f457773
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2025-10-25 16:42:54 -0300

    feat: adiciona busca por lista de categorias

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CategoryEntity } from './entities/category.entity';
+import { Repository } from 'typeorm';
+import { CreateCategory } from './dtos/create-category.dto';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(
+    @InjectRepository(CategoryEntity)
+    private readonly categoryRepository: Repository<CategoryEntity>,
+  ){}
+
+  async findAllCategories(): Promise<CategoryEntity[]> {
+    const categories = await this.categoryRepository.find();
+
+    if(!categories || categories.length === 0) {
+      throw new NotFoundException('Categories empty');
+    }
+
+    return categories;
+  }
+
+  async createCategory(
+    createCategory: CreateCategory,
+  ): Promise<CategoryEntity> {
+    return this.categoryRepository.save(createCategory);
+  }
+}
