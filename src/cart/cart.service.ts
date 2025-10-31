2ceba693fa4edd5d63a4491c9de99193713fdd7d
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2025-10-30 21:05:33 -0300

    feat: adiciona flag para verificar status do carrinho

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CartEntity } from './entities/cart.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CartService {}
+export class CartService {
+  constructor(@InjectRepository(CartEntity)
+  private readonly cartRepository: Repository<CartEntity>,
+){}
+
+  async verifyActiveCart(userId: number): Promise<CartEntity> {
+    const cart = await this.cartRepository.findOne({
+      where: {
+        userId,
+      },
+    });
+
+    if (!cart){
+      throw new NotFoundException(`Cart active not found`);
+    }
+
+    return cart;
+  }
+
+  async createCart(userId: number): Promise<CartEntity> {
+    return this.cartRepository.save({
+      active: true,
+      userId,
+    });
+  }
+}
