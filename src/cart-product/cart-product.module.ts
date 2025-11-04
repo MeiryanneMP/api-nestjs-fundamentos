fa2151d718062561a9012bb47b9ca4f8ded75777
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2025-11-04 12:03:08 -0300

    feat: Validações ao inserir carrinho

--- src/cart-product/cart-product.module.ts
+++ src/cart-product/cart-product.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CartProductEntity } from './entities/cart-product.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CartProductService } from './cart-product.service';
+import { ProductModule } from 'src/product/product.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CartProductEntity]), CartProductModule],
+  imports: [TypeOrmModule.forFeature([CartProductEntity]), ProductModule],
   providers: [CartProductService],
   exports: [CartProductService],
 })
