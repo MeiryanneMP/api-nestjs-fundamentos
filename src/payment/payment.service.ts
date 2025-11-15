1807cc80682268713d88e41a5e7c4b68d046f10d
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2025-11-15 14:55:12 -0300

    feat: adiciona funcionalidade de gerar preço final

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -3,11 +3,12 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { PaymentEntity } from './entities/payment.entity';
 import { Repository } from 'typeorm';
 import { CreateOrderDto } from 'src/order/dtos/create-order.dto';
-import { PaymentCreditCardEntity } from './entities/payment-credit-card.entity';
-import { PaymentType } from 'src/payment-status/enums/payment-type.enum';
-import { PaymentPixEntity } from './entities/payment-pix.entity';
 import { ProductEntity } from 'src/product/entities/product.entity';
 import { CartEntity } from 'src/cart/entities/cart.entity';
+import { CartProductEntity } from 'src/cart-product/entities/cart-product.entity';
+import { PaymentType } from 'src/payment-status/enums/payment-type.enum';
+import { PaymentCreditCardEntity } from './entities/payment-credit-card.entity';
+import { PaymentPixEntity } from './entities/payment-pix.entity';
 
 @Injectable()
 export class PaymentService {
@@ -16,17 +17,41 @@ export class PaymentService {
     private readonly paymentRepository: Repository<PaymentEntity>,
   ) {}
 
+  generateFinalPrice(cart: CartEntity, products: ProductEntity[]) {
+    if(!cart.cartProduct || cart.cartProduct.length === 0){
+      return 0;
+  }
+
+    return cart.cartProduct
+      .map((cartProduct: CartProductEntity) => {
+        const product = products.find(
+          (product) => product.id === cartProduct.productId,
+        );
+
+        if(product) {
+          return cartProduct.amount * product.price;
+        }
+
+        return 0;
+    })
+    .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+  }
+
   async createPayment(
     createOrderDto: CreateOrderDto,
     products: ProductEntity[],
     cart: CartEntity,
   ): Promise<PaymentEntity> {
+
+    const finalPrice = this.generateFinalPrice(cart, products);
+
     if (createOrderDto.amountPayments) {
       const paymentCreditCard = new PaymentCreditCardEntity(
         PaymentType.Done,
+        finalPrice,
         0,
-        0,
-        0,
+        finalPrice,
         createOrderDto,
       );
 
@@ -34,9 +59,9 @@ export class PaymentService {
     } else if (createOrderDto.codePix && createOrderDto.datePayment) {
       const paymentPix = new PaymentPixEntity(
         PaymentType.Done,
+        finalPrice,
         0,
-        0,
-        0,
+        finalPrice,
         createOrderDto,
       );
       return this.paymentRepository.save(paymentPix);
