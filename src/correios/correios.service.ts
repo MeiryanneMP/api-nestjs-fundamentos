457c1c145f0ee0ced87f265f306c120deb58d97d
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2025-11-17 11:26:35 -0300

    feat: busca cidades e estados do CEP

--- src/correios/correios.service.ts
+++ src/correios/correios.service.ts
@@ -1,22 +1,36 @@
 import { HttpService } from '@nestjs/axios';
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { AxiosError, AxiosResponse } from 'axios';
+import { AxiosError } from 'axios';
+import { ReturnCepExternalDto } from './dtos/return-cep-external.dto';
+import { CityService } from 'src/city/city.service';
 
 const URL_CORREIOS = 'http://viacep.com.br/ws/35164056/json';
 
 @Injectable()
 export class CorreiosService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly cityService: CityService,
+  ) {}
 
-  async findAddressByCep(cep: string): Promise<AxiosResponse<any>> {
-    return this.httpService.axiosRef.get(URL_CORREIOS.replace('{CEP}', cep))
-    .then((result) => {
-      return result.data;
-    })
-    .catch((error: AxiosError) => {
-      throw new BadRequestException(
-        `Error in connection request ${error.message}`
-      )
-    });
+  async findAddressByCep(cep: string): Promise<ReturnCepExternalDto> {
+    const returnCep: ReturnCepExternalDto = await this.httpService.axiosRef
+      .get<ReturnCepExternalDto>(URL_CORREIOS.replace('{CEP}', cep))
+      .then((result) => {
+        return result.data;
+      })
+      .catch((error: AxiosError) => {
+        throw new BadRequestException(
+          `Error in connection request ${error.message}`
+        )
+      });
+    
+
+    const city = await this.cityService.findCityByName(
+      returnCep.localidade,
+      returnCep.uf,
+    );
+    
+    return returnCep;
   }
 }
