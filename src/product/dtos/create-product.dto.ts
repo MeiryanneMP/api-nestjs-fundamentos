94e2c519c4edd09de39685ad40ff21a65c3f9af9
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2025-11-18 15:43:22 -0300

    feat: adiciona campos de calculo do frete ao inserir produto

--- src/product/dtos/create-product.dto.ts
+++ src/product/dtos/create-product.dto.ts
@@ -1,4 +1,4 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateProductDto {
   @IsNumber()
@@ -12,4 +12,24 @@ export class CreateProductDto {
 
   @IsString()
   image: string;
+
+  @IsOptional()
+  @IsNumber()
+  weight?: number;
+
+  @IsOptional()
+  @IsNumber()
+  length?: number;
+
+  @IsOptional()
+  @IsNumber()
+  height?: number;
+
+  @IsOptional()
+  @IsNumber()
+  width?: number;
+
+  @IsOptional()
+  @IsNumber()
+  diameter?: number;
 }
