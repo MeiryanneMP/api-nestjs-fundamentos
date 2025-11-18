fde36927ecf23c7cc419a422629ad55c50b6c105
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2025-11-18 15:24:11 -0300

    feat: adiciona colunas das medidas do produto para calcular frete

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -21,6 +21,21 @@ export class ProductEntity {
   @Column({ name: 'image', nullable: false })
   image: string;
 
+  @Column({ name: 'weight', nullable: false })
+  weight: number;
+
+  @Column({ name: 'length', nullable: false })
+  length: number;
+
+  @Column({ name: 'height', nullable: false })
+  height: number;
+
+  @Column({ name: 'width', nullable: false })
+  width: number;
+
+  @Column({ name: 'diameter', nullable: false })
+  diameter: number;
+
   @Column({ name: 'created_at' })
   createdAt: Date;
 
